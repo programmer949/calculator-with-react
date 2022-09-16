2c799dc497aa11d615adef480c6ccf9f1269744c
Ayxan Əliyev <85896859+programmer949@users.noreply.github.com>
2022-09-16 11:57:45 +0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import Calculator from "./Calculator";
+
+ReactDOM.render(<Calculator />, document.getElementById("root"));
